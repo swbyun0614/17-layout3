169bc1f13e13e20e666354ae8d57d0eb96c16a8c
swbyun0614 <swbyun0614@gmail.com>
2025-12-17 14:36:10 +0900

    완성

--- script/action.js
+++ script/action.js
@@ -1,18 +1,29 @@
-let number = 0
+let number = 0;
 
 $('.btn_next').click(function(){
-    number = number + 1
+    number = number + 1;
+
+    if(number == 3){
+        number = 0
+    }
+    /* if(조건){실행} - number가 3일 때 number를 0으로 만듦 */
+
     $('.hero ul').css({
         marginLeft:-number*100+'%'
-    })
-    console.log(number)
+    });
+    console.log(number);
 })
 /* .btn_next를 클릭했을 때 0이었던 변수 'number'가 1씩 더해지고, .hero 안의 ul의 marginLeft가 -(number)*100%만큼 생김 (%라는 단위는 문자이기 때문에 ''로 표시하고 숫자와 문자를 +로 이어줘야 함) */
 
 $('.btn_prev').click(function(){
-    number = number - 1
+    number = number - 1;
+
+    if(number == -1){
+        number = 2
+    }
+
     $('.hero ul').css({
         marginLeft:-number*100+'%'
-    })
-    console.log(number)
+    });
+    console.log(number);
 })
